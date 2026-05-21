16999ebed91479c7af66a02568110ba5760d5e74
Amir Hernandez <amirhernandezfarah06@gmail.com>
2026-05-21 02:26:57 -0600

    feat: add Google Tag Manager GTM-5F3C7WN2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -98,6 +98,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5F3C7WN2');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-CG871WYQHV" />
         <script
           dangerouslySetInnerHTML={{
@@ -109,6 +120,16 @@ gtag('config', 'G-CG871WYQHV');`,
         />
       </head>
       <body className="min-h-screen flex flex-col bg-slate-50 text-slate-900">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5F3C7WN2"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(orgJsonLd) }}
